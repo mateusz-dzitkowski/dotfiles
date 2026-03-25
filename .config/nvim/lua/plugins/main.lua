9fab38a0dfa8788717d8e29895c7540b70daf9f3
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-03-25 17:52:53 +0100

    move the keymaps to keymaps

--- .config/nvim/lua/plugins/main.lua
+++ .config/nvim/lua/plugins/main.lua
@@ -5,28 +5,6 @@ return {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    keys = {
-      {
-        "<leader>ff",
-        function() require("telescope.builtin").find_files() end,
-        desc = "Find files",
-      },
-      {
-        "<leader>fg",
-        function() require("telescope.builtin").live_grep() end,
-        desc = "Live grep",
-      },
-      {
-        "<leader>fb",
-        function() require("telescope.builtin").buffers() end,
-        desc = "Buffers",
-      },
-      {
-        "<leader>fh",
-        function() require("telescope.builtin").help_tags() end,
-        desc = "Help tags",
-      },
-    },
 },
 {
   "folke/which-key.nvim",
