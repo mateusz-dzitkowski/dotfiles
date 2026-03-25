52a023963f21c4068da8773dd4d6d66fd87a980e
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-03-25 18:33:38 +0100

    stuff

--- .config/nvim/lua/plugins/main.lua
+++ .config/nvim/lua/plugins/main.lua
@@ -33,6 +33,20 @@ return {
     "nvim-tree/nvim-web-devicons", -- optional, but recommended
   },
   lazy = false, -- neo-tree will lazily load itself
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          '.git',
+        },
+        never_show = {},
+      },
+    }
+  }
 },
 {
   "catppuccin/nvim",
