e8ccb555748017d9339a929277182285ef33df27
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-03-26 18:12:00 +0100

    stuff

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -26,5 +26,39 @@ return {
       vim.lsp.enable('pyright')
     end
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",      -- LSP source
+      "hrsh7th/cmp-buffer",        -- buffer completions
+      "hrsh7th/cmp-path",          -- filesystem paths
+      "saadparwaiz1/cmp_luasnip",  -- snippets
+      "L3MON4D3/LuaSnip",          -- snippet engine
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  }
 }
 
