ad73c3cd267d2be088c65dbd4576d19688ba2f11
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2026-03-27 21:47:16 +0100

    make formatter work

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,20 +6,30 @@ require("config.options")
 require("config.keymaps")
 
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = "●",
-    spacing = 2,
-  },
-  signs = true,
-  underline = true,
-  update_in_insert = true,
+	virtual_text = {
+		prefix = "●",
+		spacing = 2,
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = true,
 })
 
 require("conform").setup({
-    formatters_by_ft = {
-        python = { "ruff", "black" },
-        go = { "gofmt" },
-        javascript = { "prettier" },
-    },
+	formatters_by_ft = {
+		python = function(bufnr)
+			if require("conform").get_formatter_info("ruff_format", bufnr).available then
+				return { "ruff_fix", "ruff_format" }
+			else
+				return { "isort", "black" }
+			end
+		end,
+		go = { "gofmt" },
+		javascript = { "prettier" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
 })
-
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,9 +6,3 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function()
-        require("conform").format({ async = false })
-    end,
-})
