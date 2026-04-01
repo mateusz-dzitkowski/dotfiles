8d1d6e81042826505e4f112d61c4cfba9cccf910
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2026-04-01 13:52:05 +0200

    reformat

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,80 +1,79 @@
 return {
-  {
-    'mason-org/mason.nvim',
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-	"pyright",
-	"gopls",
-	"hls",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      vim.lsp.config('pyright', {
-        filetypes = { "python" },
-        settings = {
-          python = {
-            venvPath = ".",
-            venv = ".venv",
-            pythonPath = ".venv/bin/python",
-          },
+    {
+        'mason-org/mason.nvim',
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "pyright",
+                "gopls",
+                "hls",
+            },
         },
-      })
-
-      vim.lsp.config("gopls", {
-	filetypes = { "go" },
-      })
-
-      vim.lsp.config("hls", {
-        filetypes = { "haskell" },
-      })
-
-      vim.lsp.enable('lua_ls')
-      vim.lsp.enable('pyright')
-      vim.lsp.enable("gopls")
-      vim.lsp.enable("hls")
-    end
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",      -- LSP source
-      "hrsh7th/cmp-buffer",        -- buffer completions
-      "hrsh7th/cmp-path",          -- filesystem paths
-      "saadparwaiz1/cmp_luasnip",  -- snippets
-      "L3MON4D3/LuaSnip",          -- snippet engine
     },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.lsp.config('pyright', {
+                filetypes = { "python" },
+                settings = {
+                    python = {
+                        venvPath = ".",
+                        venv = ".venv",
+                        pythonPath = ".venv/bin/python",
+                    },
+                },
+            })
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
+            vim.lsp.config("gopls", {
+                filetypes = { "go" },
+            })
+
+            vim.lsp.config("hls", {
+                filetypes = { "haskell" },
+            })
+
+            vim.lsp.enable('lua_ls')
+            vim.lsp.enable('pyright')
+            vim.lsp.enable("gopls")
+            vim.lsp.enable("hls")
+        end
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp", -- LSP source
+            "hrsh7th/cmp-buffer", -- buffer completions
+            "hrsh7th/cmp-path",   -- filesystem paths
+            "saadparwaiz1/cmp_luasnip", -- snippets
+            "L3MON4D3/LuaSnip",   -- snippet engine
         },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-	  ["<C-e>"] = cmp.mapping.abort(),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
-    end,
-  },
-  {
-    'stevearc/conform.nvim',
-  },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                }),
+            })
+        end,
+    },
+    {
+        'stevearc/conform.nvim',
+    },
 }
-
