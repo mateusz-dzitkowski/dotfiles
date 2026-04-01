9ee0615681b3d5e2c519b18aaa4ccdea07a0bce3
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2026-04-01 13:58:02 +0200

    add some more lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -19,6 +19,12 @@ return {
                         diagnostics = {
                             globals = { "vim" },
                         },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        telemetry = {
+                            enable = false,
+                        },
                     },
                 },
             })
