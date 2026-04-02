1647da40f481a72ccae2707afa322c924eb01682
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-04-02 21:04:52 +0200

    idk sleep for a second

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# wait for the NetworkManager to start
-nm-online -q
+sleep 1
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar 2>&1 | tee -a /tmp/polybar.log & disown
