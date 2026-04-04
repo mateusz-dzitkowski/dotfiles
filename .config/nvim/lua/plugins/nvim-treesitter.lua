4f937a1a9d54f5c4d69e44a05a3b9d525ffceabf
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-04-04 17:13:22 +0200

    split the plugin files

---
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,13 @@
+return    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        lazy = false,
+        build = ":TSUpdate",
+        opts = {
+            indent = { enable = true },
+            highlight = { enable = true },
+            folds = { enable = true },
+            ensure_installed = { "diff", "json", "lua", "markdown", "python", "toml", "go" },
+            auto_install = true,
+        },
+    }
