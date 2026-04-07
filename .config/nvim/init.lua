4ade38e7e9147306422b7633208d729cdf3b8e85
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-04-07 16:20:13 +0200

    move the config to the opts in plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,28 +15,4 @@ vim.diagnostic.config({
     update_in_insert = true,
 })
 
-require("conform").setup({
-    formatters_by_ft = {
-        python = function(bufnr)
-            if require("conform").get_formatter_info("ruff_format", bufnr).available then
-                return { "ruff_fix", "ruff_format" }
-            else
-                return { "isort", "black" }
-            end
-        end,
-        go = { "gofmt", "goimports" },
-    },
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 1000,
-        lsp_format = "fallback",
-    },
-})
-
-require("nvim-treesitter.configs").setup({
-    highlight = {
-        enable = true,
-    },
-})
-
 vim.cmd.colorscheme "catppuccin-nvim"
--- .config/nvim/lua/plugins/confirm.lua
+++ .config/nvim/lua/plugins/confirm.lua
@@ -1,3 +1,20 @@
-return    {
-        "stevearc/conform.nvim",
+return {
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            python = function(bufnr)
+                if require("conform").get_formatter_info("ruff_format", bufnr).available then
+                    return { "ruff_fix", "ruff_format" }
+                else
+                    return { "isort", "black" }
+                end
+            end,
+            go = { "gofmt", "goimports" },
+        },
+        format_on_save = {
+            -- These options will be passed to conform.format()
+            timeout_ms = 1000,
+            lsp_format = "fallback",
+        },
     }
+}
