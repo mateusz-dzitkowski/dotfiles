e57e697db7aac7307c1af68519219aa967edc905
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-04-07 15:38:18 +0200

    reformat and add incremental selection

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,9 +6,9 @@ vim.keymap.del("n", "grr")
 vim.keymap.del("n", "grt")
 
 -- set some keymaps
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neotree" })
 vim.keymap.set("n", "<leader>o", ":Neotree focus<CR>", { desc = "Focus on Neotree" })
@@ -17,3 +17,20 @@ vim.keymap.set("n", "J", vim.diagnostic.open_float, { desc = "Open diagnostics" 
 
 local gitsigns = require("gitsigns")
 vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+
+-- incremental selecting
+vim.keymap.set({ "x", "o" }, "<A-o>", function()
+  if vim.treesitter.get_parser(nil, nil, { error = false }) then
+    require("vim.treesitter._select").select_parent(vim.v.count1)
+  else
+    vim.lsp.buf.selection_range(vim.v.count1)
+  end
+end, { desc = "Select parent treesitter node or outer incremental lsp selections" })
+
+vim.keymap.set({ "x", "o" }, "<A-i>", function()
+  if vim.treesitter.get_parser(nil, nil, { error = false }) then
+    require("vim.treesitter._select").select_child(vim.v.count1)
+  else
+    vim.lsp.buf.selection_range(-vim.v.count1)
+  end
+end, { desc = "Select child treesitter node or inner incremental lsp selections" })
