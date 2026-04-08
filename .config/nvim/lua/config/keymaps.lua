db77702eeab748ac78073f0f8ccfa711aa3ba963
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2026-04-08 14:40:37 +0200

    remove stnky keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,7 @@ vim.keymap.del("n", "grn")
 vim.keymap.del("n", "gra")
 vim.keymap.del("n", "grr")
 vim.keymap.del("n", "grt")
+vim.keymap.del("n", "grx")
 
 -- set some keymaps
 local builtin = require("telescope.builtin")
