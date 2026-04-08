1b2d7674bdf0a12a335c202ed84e6cb57a1bfa61
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2026-04-08 14:20:05 +0200

    fix treesitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,13 +1,22 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    "nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
-    opts = {
-        indent = { enable = true },
-        highlight = { enable = true },
-        folds = { enable = true },
-        ensure_installed = { "diff", "json", "lua", "markdown", "python", "toml", "go" },
-        auto_install = true,
-    },
+    init = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+                pcall(vim.treesitter.start)
+                vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+            end
+        })
+        
+        local ensure_installed = {"go", "gosum", "gomod", "python"}
+        local already_installed = require("nvim-treesitter.config").get_installed()
+        local parsers_to_install = vim.iter(ensure_installed)
+            :filter(function(parser)
+                return not vim.tbl_contains(already_installed, parser)
+            end)
+            :totable()
+        require("nvim-treesitter").install(parsers_to_install)
+    end,
 }
