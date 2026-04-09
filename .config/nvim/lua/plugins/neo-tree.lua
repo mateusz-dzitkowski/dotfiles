4b4a601c779cd9ba7b8ffd96fceacea07f67aee1
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2026-04-09 11:25:10 +0200

    neo tree change

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,19 +7,21 @@ return {
         "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false,                      -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config
     opts = {
         filesystem = {
             filtered_items = {
                 visible = false,
-                show_hidden_count = true,
                 hide_dotfiles = false,
                 hide_gitignored = true,
                 hide_by_name = {
                     ".git",
                 },
-                never_show = {},
             },
             use_libuv_file_watcher = true,
+            scan_mode = "deep",
+            async_directory_scan = "never",
         },
         window = {
             width = 25,
