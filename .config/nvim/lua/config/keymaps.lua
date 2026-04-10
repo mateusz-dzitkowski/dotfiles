45b872e9abab74f58abcfdf454b2ed41f794fa30
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-04-10 20:52:10 +0200

    keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -12,7 +12,16 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neotree" })
-vim.keymap.set("n", "<leader>o", ":Neotree focus<CR>", { desc = "Focus on Neotree" })
+local function toggle_neotree_focus()
+    local ft = vim.bo[vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win())].filetype
+    if ft == "neo-tree" then
+        vim.cmd("Neotree close")
+        vim.cmd("wincmd p")
+    else
+        vim.cmd("Neotree focus")
+    end
+end
+vim.keymap.set("n", "<leader>o", toggle_neotree_focus, { desc = "Focus on Neotree" })
 
 vim.keymap.set("n", "J", vim.diagnostic.open_float, { desc = "Open diagnostics" })
 
@@ -21,17 +30,17 @@ vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset hunk" })
 
 -- incremental selecting
 vim.keymap.set({ "x", "o" }, "<A-o>", function()
-  if vim.treesitter.get_parser(nil, nil, { error = false }) then
-    require("vim.treesitter._select").select_parent(vim.v.count1)
-  else
-    vim.lsp.buf.selection_range(vim.v.count1)
-  end
+    if vim.treesitter.get_parser(nil, nil, { error = false }) then
+        require("vim.treesitter._select").select_parent(vim.v.count1)
+    else
+        vim.lsp.buf.selection_range(vim.v.count1)
+    end
 end, { desc = "Select parent treesitter node or outer incremental lsp selections" })
 
 vim.keymap.set({ "x", "o" }, "<A-i>", function()
-  if vim.treesitter.get_parser(nil, nil, { error = false }) then
-    require("vim.treesitter._select").select_child(vim.v.count1)
-  else
-    vim.lsp.buf.selection_range(-vim.v.count1)
-  end
+    if vim.treesitter.get_parser(nil, nil, { error = false }) then
+        require("vim.treesitter._select").select_child(vim.v.count1)
+    else
+        vim.lsp.buf.selection_range(-vim.v.count1)
+    end
 end, { desc = "Select child treesitter node or inner incremental lsp selections" })
