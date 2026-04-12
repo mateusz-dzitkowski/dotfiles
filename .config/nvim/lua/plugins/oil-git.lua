3bb2995dc9ff830d191b26e2d57f7b27c7752296
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-04-12 12:09:32 +0200

    add oil git

---
+++ .config/nvim/lua/plugins/oil-git.lua
@@ -0,0 +1,12 @@
+return {
+    "benomahony/oil-git.nvim",
+    opts = {
+        highlights = {
+            OilGitAdded = { fg = "#a6e3a1" },     -- green
+            OilGitModified = { fg = "#f9e2af" },  -- yellow
+            OilGitRenamed = { fg = "#cba6f7" },   -- purple
+            OilGitUntracked = { fg = "#89b4fa" }, -- blue
+            OilGitIgnored = { fg = "#6c7086" },   -- gray
+        }
+    }
+}
