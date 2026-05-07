71da21fa74ea7ea93b004f1ea1acee11a97fb431
mateusz.dzitkowski <mateusz.dzitkowski.98@gmail.com>
2026-05-07 17:52:09 +0200

    stuff

---
+++ .scripts/setup_keyboard.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xset r rate 300 40
+setxkbmap -layout pl,us -option caps:escape
+
---
+++ .scripts/setup_mouse.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xinput set-prop 'SteelSeries SteelSeries Rival 3' 'libinput Accel Profile Enabled' 0 1 0
+xinput set-prop 'SteelSeries SteelSeries Rival 3' 'libinput Accel Speed' 0.2
+
